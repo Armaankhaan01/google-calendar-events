489d4e7f78196c13752de7ce0b56fb9278f01cab
Armaankhaan01 <armankhan876426@gmail.com>
2025-01-22 16:40:55 +0530

    Fixed: SIGTERM ERROR

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
 const authRoutes = require("./routes/authRoutes");
 const calendarRoutes = require("./routes/calendarRoutes");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -29,8 +27,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Start server
-app.listen(PORT, () => {
+// Start server and store the server instance
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
