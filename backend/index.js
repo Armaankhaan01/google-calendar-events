4e82fc8b2528adbe33897f7812ef447c74669618
Armaankhaan01 <armankhan876426@gmail.com>
2025-01-22 16:23:19 +0530

    Fix: Type Error in cors policy

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://calendar-events.arrmankhan.me", "http://localhost:5173"],
+    origin: ["https://calendar-events.armankhan.me", "http://localhost:5173"],
   })
 );
 app.use(express.json());
